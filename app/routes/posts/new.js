d805295a556b1fb59cb34766580eb8435a030cdd
Jimmy Thigpen <jmthigpen@me.com>
2015-03-02 09:43:59 -0500

    9AM

--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     createPost: function(newTitle, newBody) {
+      // console.log(this.model);
       var self = this;
       var data = {title: newTitle, body: newBody};
       Ember.$.ajax("https://api.parse.com/1/classes/Post/", {
@@ -14,6 +15,9 @@ export default Ember.Route.extend({
       }).done(function(){
         self.modelFor('posts').addObject(data);
         self.transitionTo('posts');
+        // self.get('model').addObject(data);
+        // self.set('newTitle', '');
+        // Ember.$('.new-title').val("");
       });
     }
   },
